3b451a624cf85e6b10d5cadac4726e9df9e24051
Ramindur Singh <ramindurs@gmail.com>
2024-03-06 10:36:58 +0000

    RELEASE 0.11.0 - Using Simex-Messaging 0.8.0

--- src/test/scala/shareprice/config/ConfigurationTest.scala
+++ src/test/scala/shareprice/config/ConfigurationTest.scala
@@ -5,8 +5,12 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import cats.effect.unsafe.implicits.global
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
 
 class ConfigurationTest extends AnyFlatSpec with Matchers with ScalaFutures {
+
+  implicit val defaultPatience =
+    PatienceConfig(timeout = Span(30, Seconds), interval = Span(100, Millis))
 
   it should "load configuration" in {
     val result = Configuration.loadAsStream[IO]().compile.drain.unsafeToFuture()
